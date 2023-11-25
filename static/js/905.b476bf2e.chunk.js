3cf792850aa093813888cda0add4179d7e63c57a
AnnaFesenko7 <90966976+AnnaFesenko7@users.noreply.github.com>
2023-11-25 14:31:53 +0000

    Deploying to gh-pages from @ AnnaFesenko7/future-generation@1b03350576e743c116722237ceae08e52fc949fa 🚀

---
+++ static/js/905.b476bf2e.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkfuture_generation=self.webpackChunkfuture_generation||[]).push([[905],{905:function(e,u,n){n.r(u);n(184);u.default=function(){}}}]);
+//# sourceMappingURL=905.b476bf2e.chunk.js.map
